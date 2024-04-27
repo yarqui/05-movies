c395fb453c86495cfca66926e9e47e56f8f7a0d4
yarqui <y.pelykh@gmail.com>
2024-04-26 22:47:43 -0400

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-woolf-hw-05-movies",
+  base: "/",
   plugins: [react()],
 });
