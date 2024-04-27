c9a9084e124a6a7a376440a3329b0af87eafc649
yarqui <y.pelykh@gmail.com>
2024-04-27 16:31:35 -0400

    add movie datails page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import { Toaster } from "react-hot-toast";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 // const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
-// const MovieDetailsPage = lazy(
-//   () => import("./pages/MovieDetailsPage/MovieDetailsPage.jsx"),
-// );
-// const Cast = lazy(() => import("./components/Cast/Cast"));
-// const Reviews = lazy(() => import("./components/Reviews/Reviews"));
+const MovieDetailsPage = lazy(
+  () => import("./pages/MovieDetailsPage/MovieDetailsPage.jsx"),
+);
+const Cast = lazy(() => import("./components/Cast/Cast"));
+const Reviews = lazy(() => import("./components/Reviews/Reviews"));
 const NotFoundPage = lazy(
   () => import("./pages/NotFoundPage/NotFoundPage.jsx"),
 );
@@ -25,10 +25,10 @@ function App() {
 
           {/* <Route path={PAGE_NAMES.movies} element={<MoviesPage />} /> */}
 
-          {/* <Route path={PAGE_NAMES.movieDetails} element={<MovieDetailsPage />}>
+          <Route path={PAGE_NAMES.movieDetails} element={<MovieDetailsPage />}>
             <Route path={PAGE_NAMES.cast} element={<Cast />} />
             <Route path={PAGE_NAMES.reviews} element={<Reviews />} />
-          </Route> */}
+          </Route>
 
           <Route path="*" element={<NotFoundPage />} />
         </Route>
