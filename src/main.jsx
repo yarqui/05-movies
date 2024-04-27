7ee31a3a46cf61009ed3cfde16420732dd1612c6
yarqui <y.pelykh@gmail.com>
2024-04-27 13:41:13 -0400

    edit main.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-import { LoadingProvider } from "./components/LoadingProvider/LoadingProvider.jsx";
+// import { LoadingProvider } from "./components/LoadingProvider/LoadingProvider.jsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LoadingProvider>
-        <App />
-      </LoadingProvider>
+      {/* <LoadingProvider> */}
+      <App />
+      {/* </LoadingProvider> */}
     </BrowserRouter>
   </React.StrictMode>,
 );
